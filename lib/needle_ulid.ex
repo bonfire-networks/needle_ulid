af5d148b254fc22e6ae53e859141c46148f7414b
Mayel de Borniol <git@mayel.space>
2025-05-17 23:32:57 +0100

    as_uuid

--- lib/needle_ulid.ex
+++ lib/needle_ulid.ex
@@ -176,6 +176,13 @@ defmodule Needle.ULID do
 
   defp random(), do: :crypto.strong_rand_bytes(10)
 
+
+def as_uuid(ulid) do
+  with {:ok, id} <- dump(ulid) do
+     Uniq.UUID.to_string(id, :default)
+  end
+end
+
   @doc """
   Generates a Crockford Base32 encoded ULID.
 
